b9271a2de4158fd5b82dc06ca5ccf64c1238710e
mustafasahar77 <mustafasahar7@gmail.com>
2026-06-28 20:31:16 -0700

    Update page.tsx
    corrected names

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -44,9 +44,9 @@ const values = [
 
 const team = [
   { name: "Mustafar Sahar", title: "Founder & CEO", image: "/team/mustafar-sahar.jpg" },
-  { name: "Mujataba Sahar", title: "Operations Director", image: "/team/mujataba-sahar.jpg" },
+  { name: "Mujtaba Sahar", title: "Operations Director", image: "/team/mujataba-sahar.jpg" },
   { name: "Samiullah Mohib", title: "Sales Manager", image: "/team/samiullah-mohib.jpg" },
-  { name: "Sijad Liwal", title: "Sales Associate", image: "/team/sijad-liwal.jpg" },
+  { name: "Sajjad Liwal", title: "Sales Associate", image: "/team/sijad-liwal.jpg" },
 ];
 
 const mapEmbed = `https://www.google.com/maps?q=${encodeURIComponent(
